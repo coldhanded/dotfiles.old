f679d1398253cdf550ac3de8078b1965f846f92b
Cold Hands <coldhandsthegod@gmail.com>
2021-01-12 05:59:14 -0500

    still cleaning

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env sh
 
-## Add this to your wm startup file.
-
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# New Bar
+# Launch Bar
 polybar simple -c ~/.config/polybar/config &
