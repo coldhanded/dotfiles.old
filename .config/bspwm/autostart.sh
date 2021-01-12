3525b5848ed34a181ba0dc52c78b778486f0f198
Cold Hands <coldhandsthegod@gmail.com>
2021-01-11 23:35:27 -0500

    cleanup

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -1,10 +1,3 @@
-#   _________  __   ___  __ _____   _  _____  ____
-#  / ___/ __ \/ /  / _ \/ // / _ | / |/ / _ \/ __/
-# / /__/ /_/ / /__/ // / _  / __ |/    / // /\ \  
-# \___/\____/____/____/_//_/_/ |_/_/|_/____/___/ 
-# coldhands.xyz
-# 2021
-
 #!/bin/bash
 
 function run {
@@ -14,18 +7,16 @@ function run {
   fi
 }
 
-# Polybar
 #$HOME/.config/polybar/launch.sh &
 $HOME/polybar/launch.sh &
 
-# Autostarts
 xsetroot -cursor_name left_ptr &
 run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
 numlockx on &
+blueberry-tray &
 picom --config $HOME/.config/bspwm/picom.conf &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 run dropbox &
 run discord &
 run dunst &
 wal -R &
-$HOME/.scripts/reload_dunst.sh &
-
