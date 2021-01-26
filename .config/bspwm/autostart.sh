bdc29a03d02a93085f315f402db0e0c6440a08f0
Cold Hands <coldhandsthegod@gmail.com>
2021-01-26 03:36:14 -0500

    new

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -17,8 +17,9 @@ $HOME/.config/polybar/launch.sh &
 
 xsetroot -cursor_name left_ptr &
 run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
+bsp-layout set tall I
+bsp-layout set monocle V
 blueberry-tray &
 picom --config $HOME/.config/picom/picom.conf &
-run dropbox &
 wal -R &
 $HOME/.scripts/reload_dunst.sh &
